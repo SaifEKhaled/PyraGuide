1a00c36fc094024b505a8e02de8a241ca4049ec4
Saif Elden Khaled <saifkhaledd1511@gmail.com>
2023-12-01 22:33:35 +0200

    Flutter Sign Up page -not the final implementation,more like a placeholder-

---
+++ android/app/src/main/kotlin/com/example/softwaretest/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.softwaretest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: AuthenticationScreen(),
+    );
+  }
+}
+
+class AuthenticationScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your App Name'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Sign-up Section
+            TextField(
+              decoration: InputDecoration(labelText: 'Name'),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(labelText: 'Password'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Implement sign-up logic
+              },
+              child: Text('Sign Up'),
+            ),
+
+            // Divider or some spacing between Sign-up and Login sections
+
+            // Login Section
+            TextField(
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(labelText: 'Password'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Implement login logic
+              },
+              child: Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
